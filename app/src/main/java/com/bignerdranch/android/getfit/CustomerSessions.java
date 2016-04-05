884b2aa2328775fd79c6c73a038f16db74e22121
golub71 <golub71@gmail.com>
2016-04-05 14:48:57 -0400

    New Activities

--- app/src/main/java/com/bignerdranch/android/getfit/CustomerSessions.java
+++ app/src/main/java/com/bignerdranch/android/getfit/CustomerSessions.java
@@ -2,13 +2,12 @@ package com.bignerdranch.android.getfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 
-public class User extends AppCompatActivity {
+public class CustomerSessions extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+        setContentView(R.layout.activity_customer_sessions);
     }
 }
